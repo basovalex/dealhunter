39ada88acad6263988a5eb6cb3f17e5361fb4f01
basovalex <basovalex@gmail.com>
2026-06-15 16:08:19 +0500

    Register models in Django admin

--- games/admin.py
+++ games/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Game, PriceSnapshot, Store, Watchlist
+
+
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'itad_store_id']
+    search_fields = ['name', 'slug']
+
+
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'steam_appid', 'historical_low', 'created_at']
+    search_fields = ['title', 'itad_id']
+    list_filter = ['created_at']
+
+
+@admin.register(PriceSnapshot)
+class PriceSnapshotAdmin(admin.ModelAdmin):
+    list_display = ['game', 'store', 'price', 'regular_price', 'cut', 'recorded_at']
+    search_fields = ['game__title', 'store__name']
+    list_filter = ['store', 'recorded_at']
+
+
+@admin.register(Watchlist)
+class WatchlistAdmin(admin.ModelAdmin):
+    list_display = ['user', 'game', 'target_price', 'is_notified', 'created_at']
+    search_fields = ['user__username', 'game__title']
+    list_filter = ['is_notified']
